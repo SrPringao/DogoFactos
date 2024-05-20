dcd2c640550ef37c02fb865ddb0dc955859ad6b6
francoaldretee <franco@intelica.mx>
2024-05-20 03:06:37 -0600

    Initial Commit

---
+++ DogFacts/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DogFacts
+//
+//  Created by Franco Aldrete on 20/05/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
